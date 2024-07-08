85b1f205c62fdaa5c08441fad93cfb09d8dd22b7
tooggle <114974128+tooggle@users.noreply.github.com>
2024-07-08 10:48:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ if st.sidebar.checkbox('Load Model'):
 
     # Image
     if options == 'Image':
-        option1 = st.selectbox(
+        option1 = st.sidebar.selectbox(
              'you can select some image',
              ('image_1', 'image_2'))
         upload_img_file = st.sidebar.file_uploader(
